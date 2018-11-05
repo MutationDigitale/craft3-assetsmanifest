7e3680a74e165528a8bd9c0181bb94fdb29ee458
smcyr@hotmail.comsmcyr@hotsmcyr@hotmail.commail.com <smcyr@hotmail.com>
2018-11-04 19:01:29 -0500

    1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 1.0.2 - 2018-11-04
+
+### Fixed
+- Don't crash when there is no manifest.json
+
 ## 1.0.0 - 2018-11-02
 
 ### Added
--- src/variables/AssetsVariable.php
+++ src/variables/AssetsVariable.php
@@ -2,6 +2,7 @@
 
 namespace mutation\assetsmanifest\variables;
 
+use Exception;
 use mutation\assetsmanifest\AssetsManifestPlugin;
 use mutation\assetsmanifest\models\SettingsModel;
 
@@ -13,9 +14,18 @@ class AssetsVariable
         $settings = AssetsManifestPlugin::$plugin->getSettings();
 
         $manifestPath = $this->normalizePath(CRAFT_BASE_PATH . '/' . $settings->manifestPath);
-        $manifest = file_get_contents($manifestPath);
-        $json = json_decode($manifest, true);
-        return $json['version'];
+        try {
+            $manifest = file_get_contents($manifestPath);
+
+            if ($manifest) {
+                $json = json_decode($manifest, true);
+                return $json['version'];
+            }
+        } catch (Exception $e) {
+
+        }
+
+        return time();
     }
 
     private function normalizePath($path)
